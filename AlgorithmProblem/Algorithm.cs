f1ea863e9fbc0170f409b02d8123b6d929970473
mohan338 <sahumohan338@gmail.com>
2022-05-08 15:09:02 +0530

    [mohan]add: binary search in string

---
+++ AlgorithmProblem/Algorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    public class Algorithm
+    {
+        public int Binary(string[] arr, int start, int end, string key)
+        {
+            while (start <= end)
+            {
+                int Midvalue = (start + end) / 2;
+                if (key.CompareTo(arr[Midvalue]) == 0)
+                {
+                    return Midvalue;
+                }
+                else if (key.CompareTo(arr[Midvalue]) > 0)
+                {
+                    start=Midvalue+1;
+                }
+                else
+                {
+                    end=Midvalue-1;
+                }
+
+            }
+            return -1;
+        }
+    }
+}
---
+++ AlgorithmProblem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AlgorithmProblem;
+public class Program
+{
+    const string File_Path = @"D:\dotnet\algorithms\AlgorithmPrograms\AlgorithmProblem\Utility\Element.txt";
+    public static void Main(String[] args)
+    {
+        bool check=true;
+        while(check)
+        {
+            Console.WriteLine("press 1 to display binary search");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+               case 1:
+                    Algorithm problem = new Algorithm();
+                    string Input_File = File.ReadAllText(File_Path);
+                    string[] arr = Input_File.Split(',');
+                    Console.WriteLine("Enter word to search");
+                    int lengthvalue=arr.Length;
+                    string key = Console.ReadLine();
+                    int result = problem.Binary(arr,0, lengthvalue - 1,key);
+                    if(result == -1)
+                    {
+                        Console.WriteLine(key+"word is present");
+                    }
+                    else
+                    {
+                        Console.WriteLine(key + "word is not present");
+                    }
+                    break;
+                case 0:
+                    check = false;
+                    break;
+            }
+        }
+    }
+}
